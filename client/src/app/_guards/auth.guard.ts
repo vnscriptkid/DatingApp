5246125134684909986a6ffbfe6479b835790969
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-20 23:18:05 +0700

    Guard routes from unauthorized user

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, TimeoutError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map((user) => {
+        if (user) return true;
+        this.toastr.error('You are not authorized');
+        return false;
+      })
+    )
+  }
+  
+}
