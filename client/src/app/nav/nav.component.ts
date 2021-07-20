985dfa67f542b20dd060c35994cc1820a09229e7
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-20 14:07:17 +0700

    Post login creds to login api
    - Init account service
    - Inject it into nav component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  creds: any = {};
+  model: any = {};
+  loggedIn = false;
   
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log('login: ', this.creds);
+    this.accountService.login(this.model)
+      .subscribe(response => {
+        console.log(response);
+        this.loggedIn = true;
+      }, error => {
+        console.error(error);
+      })
   }
 }
