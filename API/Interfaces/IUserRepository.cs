d48c103ac396af9e5bf42f20c8d886cac2a2bf17
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-22 18:51:20 +0700

    [API] Add user repo, setup automapper

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetAllUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsername(string Username);
+    }
+}
