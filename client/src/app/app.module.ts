12d292495777f30ad31db8346a878e5be842b168
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-23 00:31:35 +0700

    [NG] Add MemberCard, render it inside MemberList

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
     NotFoundComponent,
     ServerErrorComponent,
     MemberListComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
