62f19c31b6e346f8f95c158608e8b3d18db8c663
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-24 14:33:46 +0700

    [NG] Add PhotoEditor to show all member's photos

---
+++ client/src/app/photo-editor/photo-editor.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from '../_models/Member';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+
+  @Input() member: Member;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
