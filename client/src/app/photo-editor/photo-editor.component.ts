1cd3417223c2c0faa2c5874f6507c7a267882075
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-24 18:37:14 +0700

    [NG] Install file-upload lib, hook it up with PhotoEditor

--- client/src/app/photo-editor/photo-editor.component.ts
+++ client/src/app/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Member } from '../_models/Member';
+import { User } from '../_models/User';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,10 +14,46 @@ import { Member } from '../_models/Member';
 export class PhotoEditorComponent implements OnInit {
 
   @Input() member: Member;
+
+  baseUrl = `${environment.apiUrl}/users/add-photo`;
+
+  hasBaseDropZoneOver:boolean;
+  uploader: FileUploader;
+  user: User;
   
-  constructor() { }
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+    this.initializeUploader();
+  }
 
   ngOnInit(): void {
   }
 
+  fileOverBase(event: any): void {
+    this.hasBaseDropZoneOver = event;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl,
+      authToken: `Bearer ${this.user.token}`,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: false,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
+  }
 }
+
