2172319c95fafad9fad76ffabeea42eb30f6ce91
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-26 20:48:00 +0700

    [API] Login endpoint should return photoUrl

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,17 +15,19 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IUserRepository _userRepository;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IUserRepository userRepository)
         {
             _context = context;
             _tokenService = tokenService;
+            _userRepository = userRepository;
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
-            var user = await _context.Users
-                .SingleOrDefaultAsync(u => u.Username == loginDto.Username);
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -40,7 +43,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.Username,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
             };
         }
 
