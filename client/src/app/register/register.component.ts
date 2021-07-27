cf21afa8f3189fe8391b0fad2e44d6a41004ede2
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-27 08:40:19 +0700

    [NG] Add validations for register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -21,10 +20,20 @@ export class RegisterComponent implements OnInit {
   }
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl(''),
-      passwordConfirm: new FormControl('')
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
     });
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      // @ts-ignore
+      return control?.value === control?.parent?.controls[matchTo].value ? null : { isMatching: true };
+    }
   }
 
   register() {
