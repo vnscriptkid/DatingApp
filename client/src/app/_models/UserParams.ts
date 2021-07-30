d657cc51b972bdc2ee193a02ff61b8243c368d60
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-30 20:13:54 +0700

    [NG] Update member list UI with orderBy
    - Register ButtonsModule from ngx-bootstrap in shared.module
    - Update UserParams, allowing passing along orderBy as query param
    - Update member-list html to include 2 buttons, to filter by lastActive or created

--- client/src/app/_models/UserParams.ts
+++ client/src/app/_models/UserParams.ts
@@ -1,15 +1,22 @@
+enum UserOrderBy {
+    LAST_ACTIVE = 'lastActive',
+    CREATED = 'created'
+}
+
 export class UserParams {
     minAge: number;
     maxAge: number;
     pageNumber: number;
     pageSize: number;
     gender: string;
+    orderBy: UserOrderBy;
 
-    constructor(gender: string, minAge = 18, maxAge = 99, pageNumber = 1,pageSize = 5) {
+    constructor(gender: string, minAge = 18, maxAge = 99, pageNumber = 1,pageSize = 5, orderBy=UserOrderBy.LAST_ACTIVE) {
         this.gender = gender;
         this.minAge = minAge;
         this.maxAge = maxAge;
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
+        this.orderBy = orderBy;
     }
 }
