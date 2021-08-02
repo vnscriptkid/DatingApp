6e9b80d97eb583c21de2cdb3400772961ce36447
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-02 18:40:18 +0700

    [API] Create singleton service that stores online users

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -9,14 +9,33 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _presenceTracker;
+
+        public PresenceHub(PresenceTracker presenceTracker)
+        {
+            _presenceTracker = presenceTracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await _presenceTracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var onlineUsers = await _presenceTracker.GetOnlineUsers();
+
+            await Clients.All.SendAsync("GetOnlineUsers", onlineUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _presenceTracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
+
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            var onlineUsers = await _presenceTracker.GetOnlineUsers();
+
+            await Clients.All.SendAsync("GetOnlineUsers", onlineUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
