eb8394aced16363db3be03b5f14c3ddd7f0f072d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-03 23:59:45 +0700

    [NG] Get notified as message coming

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -1,8 +1,10 @@
 
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/User';
 
@@ -18,7 +20,7 @@ export class PresenceService {
 
   public onlineUsernames$ = this.onlineUsersSource.asObservable();
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private router: Router) { }
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -41,6 +43,13 @@ export class PresenceService {
     this.hubConnection.on('GetOnlineUsers', (onlineUsernames: string[]) => {
       this.onlineUsersSource.next(onlineUsernames);
     });
+
+    this.hubConnection.on('NewMessageReceived', ({ username, knownAs }) => {
+      this.toastr.info(`${knownAs} has sent you a message`)
+        .onTap
+        .pipe(take(1))
+        .subscribe(() => this.router.navigateByUrl(`/members/${username}?tab=3`));
+    })
   }
 
   stopHubConnection() {
