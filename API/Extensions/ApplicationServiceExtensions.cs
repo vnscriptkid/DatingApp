d1ebd5218f6e497b8ef7ac23f680eb8f39a1c226
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-05 21:04:19 +0700

    [API] Fix configs duplicated

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -23,11 +23,6 @@ namespace API.Extensions
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddDbContext<DataContext>(options =>
-            {
                 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
                 string connStr;
@@ -62,7 +57,6 @@ namespace API.Extensions
                 // or from the environment variable from Heroku, use it to set up your DbContext.
                 options.UseNpgsql(connStr);
             });
-
 
             return services;
         }
